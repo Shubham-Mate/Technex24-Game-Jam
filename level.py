3bafe49215fb3fc56724a4b50de0ce04658127db
Shubham-Mate <73774843+Shubham-Mate@users.noreply.github.com>
2024-03-09 09:37:00 +0530

    Add files via upload
    Created feature for multiple levels

--- level.py
+++ level.py
@@ -1,8 +1,33 @@
 import pytmx
+from player import Player
+from camera import Camera
+import numpy as np
 from pytmx.util_pygame import load_pygame
 
 class Level:
-    def __init__(self, screen_size, tile_size, level_name):
+    def __init__(self, screen_size, tile_size, level_name, color):
         self.screen_size = screen_size
         self.tile_size = tile_size
         self.level = load_pygame("test1.tmx")
+        self.p1 = Player(np.array([0, 0], dtype='float32'), np.array([32, 32]), "dummy")
+        self.c1 = Camera(0, 0)
+        self.color_identifier = color
+
+
+class LevelManager:
+    def __init__(self, l1, l2, l3, l4):
+        self.levels = [l1, l2, l3, l4]
+        self.levelInd = 0
+
+    def nextLevel(self):
+        self.levelInd += 1
+        if self.levelInd >= 4:
+            self.levelInd = 0
+
+    def prevLevel(self):
+        self.levelInd -= 1
+        if self.levelInd <= -1:
+            self.levelInd = 3
+
+    
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from player import Player
 from level import Level
 from camera import Camera
+from level import LevelManager
  
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -21,17 +22,10 @@ pygame.display.set_caption("pygame Test")
 # clock is used to set a max fps
 clock = pygame.time.Clock()
 
-# Create a player
-p1 = Player(np.array([0, 0], dtype='float32'), np.array([32, 32]), "dummy")
 GRAVITY = 3
 
-# Create the level
-l1 = Level(screen_size, TILE_SIZE, "./test1.tmx")
-
-# Create camera
-c1 = Camera(0, 0)
-c1.set_scroll_speed(np.array([0, 0]))
-print(((TILE_SIZE[0]/2) + 640)/20)
+# Create the level manager
+lm1 = LevelManager(Level(screen_size, TILE_SIZE, "./test1.tmx", pygame.Color(235, 113, 26)), Level(screen_size, TILE_SIZE, "./test1.tmx", pygame.Color(68, 235, 26)), Level(screen_size, TILE_SIZE, "./test1.tmx", pygame.Color(24, 107, 222)), Level(screen_size, TILE_SIZE, "./test1.tmx", pygame.Color(224, 43, 155)))
 
 
 def collision_test(rect, tiles):
@@ -49,11 +43,11 @@ def move(rect, movement, tiles):
         if movement[0] > 0:
             rect.right = tile.left
             collision_types['right'] = True
-            c1.set_scroll_speed(np.array([0, 0]))
+            lm1.levels[lm1.levelInd].c1.set_scroll_speed(np.array([0, 0]))
         elif movement[0] < 0:
             rect.left = tile.right
             collision_types['left'] = True
-            c1.set_scroll_speed(np.array([0, 0]))
+            lm1.levels[lm1.levelInd].c1.set_scroll_speed(np.array([0, 0]))
     rect.y += movement[1]
     hit_list = collision_test(rect, tiles)
     for tile in hit_list:
@@ -70,34 +64,38 @@ def move(rect, movement, tiles):
 running = True
 while running:
     dt = clock.tick(60)
-    c1.add_scroll(p1.getRect(), TILE_SIZE)
+    lm1.levels[lm1.levelInd].c1.add_scroll(lm1.levels[lm1.levelInd].p1.getRect(), TILE_SIZE)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.RESIZABLE:
             screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_j:
+                lm1.prevLevel()
+            if event.key == pygame.K_l:
+                lm1.nextLevel()
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        p1.set_speed(np.array([0, -1]))
+        lm1.levels[lm1.levelInd].p1.set_speed(np.array([0, -1]))
 
     if keys[pygame.K_s]:
-        p1.set_speed(np.array([0, 1]))
+        lm1.levels[lm1.levelInd].p1.set_speed(np.array([0, 1]))
 
     if keys[pygame.K_a]:
-        p1.set_speed(np.array([-1, 0]))
+        lm1.levels[lm1.levelInd].p1.set_speed(np.array([-1, 0]))
 
     if keys[pygame.K_d]:
-        p1.set_speed(np.array([1, 0]))
+        lm1.levels[lm1.levelInd].p1.set_speed(np.array([1, 0]))
 
-
-     
+    print(lm1.levelInd)
 
     # Gravity Physics
-    if p1.speed[1] >= 6:
-        p1.set_speed(np.array([p1.speed[0], p1.speed[1]]))
+    if lm1.levels[lm1.levelInd].p1.speed[1] >= 6:
+        lm1.levels[lm1.levelInd].p1.set_speed(np.array([lm1.levels[lm1.levelInd].p1.speed[0], lm1.levels[lm1.levelInd].p1.speed[1]]))
     else:
-        p1.set_speed(np.array([p1.speed[0], p1.speed[1] + (GRAVITY * dt/1000)]))
+        lm1.levels[lm1.levelInd].p1.set_speed(np.array([lm1.levels[lm1.levelInd].p1.speed[0], lm1.levels[lm1.levelInd].p1.speed[1] + (GRAVITY * dt/1000)]))
 
     
 
@@ -106,20 +104,20 @@ while running:
 
     # Draw the level
     tile_rect = []
-    for layer in l1.level.layers:
+    for layer in lm1.levels[lm1.levelInd].level.layers:
         for x, y, surf in layer.tiles():
-            screen.blit(surf, dest=(x*TILE_SIZE[0] - c1.pos[0], y*TILE_SIZE[1] - c1.pos[1]))
+            screen.blit(surf, dest=(x*TILE_SIZE[0] - lm1.levels[lm1.levelInd].c1.pos[0], y*TILE_SIZE[1] - lm1.levels[lm1.levelInd].c1.pos[1]))
             tile_rect.append(pygame.Rect(x*TILE_SIZE[0], y*TILE_SIZE[1], TILE_SIZE[0], TILE_SIZE[1]))
 
-    p1.rectangle, collisions = move(p1.getRect(), p1.speed * p1.speedMultiplier, tile_rect)
+    lm1.levels[lm1.levelInd].p1.rectangle, collisions = move(lm1.levels[lm1.levelInd].p1.getRect(), lm1.levels[lm1.levelInd].p1.speed * lm1.levels[lm1.levelInd].p1.speedMultiplier, tile_rect)
 
     if collisions['bottom']:
-        p1.set_speed(np.array([p1.speed[0], 0]))
+        lm1.levels[lm1.levelInd].p1.set_speed(np.array([lm1.levels[lm1.levelInd].p1.speed[0], 0]))
 
 
      
     # draw to the screen
-    pygame.draw.rect(screen, pygame.Color(255, 255, 255), p1.getRect().move(-c1.pos[0], 0))
+    pygame.draw.rect(screen, pygame.Color(255, 255, 255), lm1.levels[lm1.levelInd].p1.getRect().move(-lm1.levels[lm1.levelInd].c1.pos[0], 0))
  
 
     # flip() updates the screen to make our changes visible
