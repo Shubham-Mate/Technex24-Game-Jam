7d168dd401c91994ac2d8253d8f004e102228e27
Shubham-Mate <73774843+Shubham-Mate@users.noreply.github.com>
2024-03-09 15:01:21 +0530

    Update pixil-frame-0(4).tsx

--- pixil-frame-0(4).tsx
+++ pixil-frame-0(4).tsx
@@ -2,6 +2,6 @@
 <tileset version="1.10" tiledversion="1.10.2" name="pixil-frame-0(4)" tilewidth="64" tileheight="64" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="64" height="64" source="../../../../../Downloads/pixil-frame-0(4).png"/>
+  <image width="64" height="64" source="./pixil-frame-0(4).png"/>
  </tile>
 </tileset>
