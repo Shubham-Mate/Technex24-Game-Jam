4f029215e0cd74b1cb028f8775dc7bbb1a72ab36
Shubham-Mate <73774843+Shubham-Mate@users.noreply.github.com>
2024-03-09 14:47:10 +0530

    Add files via upload

---
+++ Industrial tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Industrial tileset" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="5. Industrial Tileset - Free/1_Industrial_Tileset_1.png" width="192" height="128"/>
+</tileset>
