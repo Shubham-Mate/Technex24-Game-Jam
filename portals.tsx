574ea2b10cb3dff477f2b9036665819a84debece
Shubham-Mate <73774843+Shubham-Mate@users.noreply.github.com>
2024-03-13 11:50:55 +0530

    Delete portals.tsx

--- portals.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="portals" tilewidth="32" tileheight="32" tilecount="16" columns="8">
- <image source="portal_tileset.png" width="256" height="64"/>
-</tileset>
