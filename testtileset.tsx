2b84695ef938876d66b12668046476a00f1d2ace
Shubham-Mate <73774843+Shubham-Mate@users.noreply.github.com>
2024-03-13 11:52:36 +0530

    Delete testtileset.tsx

--- testtileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="testtileset" tilewidth="32" tileheight="32" tilecount="264" columns="24">
- <image source="Complete_GUI_Essential_Pack_Free_Version/01_Basic_Collection/01_Flat_Theme/Spritesheets/Spritesheet_UI_Flat.png" width="768" height="352"/>
-</tileset>
