6b6454ef3bf19778795cf021b381547c88253a6c
Shubham-Mate <73774843+Shubham-Mate@users.noreply.github.com>
2024-03-13 11:51:15 +0530

    Delete teleporter.tsx

--- teleporter.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.10.2" name="teleporter" tilewidth="43" tileheight="64" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="43" height="64" source="teleporter.png"/>
- </tile>
-</tileset>
