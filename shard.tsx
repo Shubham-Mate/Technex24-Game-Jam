9156171e04200b890284bc79e66278df59df74a2
Shubham-Mate <73774843+Shubham-Mate@users.noreply.github.com>
2024-03-13 11:51:37 +0530

    Delete shard.tsx

--- shard.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.10.2" name="shard" tilewidth="14" tileheight="14" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="14" height="14" source="shard.png"/>
- </tile>
-</tileset>
